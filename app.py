70edcd241c47aa694c6a29b76f6ca08b0d4e0875
이강 <tworiver@igangs-MacBook-Air.local>
2024-08-05 21:39:59 +0900

    yolo

--- app.py
+++ app.py
@@ -8,11 +8,38 @@ from PIL import Image
 from rembg.bg import fix_image_orientation, naive_cutout, get_concat_v_multi
 import os
 import tempfile
+from ultralytics import YOLO
 
 app = FastAPI()
 
-model_dir = 'u2net_human_seg'
-session = new_session(model_dir)
+yolo_model = YOLO('yolov8n.pt')
+
+model_name = 'DIS'
+session = new_session(model_name)
+
+
+def crop_human(img, model, expand_ratio=0.2):
+    results = model(img)
+
+    human_boxes = [r for r in results[0].boxes if r.cls == 0]
+    if human_boxes:
+        x1, y1, x2, y2 = map(int, human_boxes[0].xyxy[0])
+
+        # Expand bounding box size by the specified ratio
+        width_expansion = int((x2 - x1) * expand_ratio)
+        height_expansion = int((y2 - y1) * expand_ratio)
+
+        # Calculate new coordinates with expansion
+        x1 = max(0, x1 - width_expansion)
+        y1 = max(0, y1 - height_expansion)
+        x2 = min(img.shape[1], x2 + width_expansion)
+        y2 = min(img.shape[0], y2 + height_expansion)
+
+        cropped_img = img[y1:y2, x1:x2]
+        return cropped_img, (x1, y1, x2, y2)
+    else:
+        return None, None
+    
 
 def extract_edges(data, session):
     img = Image.fromarray(data)
@@ -31,6 +58,20 @@ def extract_edges(data, session):
     
     return np.asarray(cutout), np.asarray(mask)
 
+def paste_on_original_size(cropped_img, orig_size, bbox, is_edge=False):
+    # Create a transparent RGBA image with the original image size
+    full_img = np.zeros((orig_size[0], orig_size[1], 4), dtype=np.uint8)
+    
+    # Get bounding box coordinates
+    x1, y1, x2, y2 = bbox
+    
+    # Place the cropped image into the original size canvas
+    if not is_edge:
+        cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2RGBA)
+    full_img[y1:y2, x1:x2] = cropped_img
+    
+    return full_img
+
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     img_bytes = await file.read()
@@ -41,32 +82,43 @@ async def predict(file: UploadFile = File(...)):
     img = cv2.imread(temp_file_path)
     os.remove(temp_file_path)
 
-    output, mask = extract_edges(img, session=session)
+    cropped_img, bbox = crop_human(img, yolo_model)
+    if cropped_img is None:
+        return {
+        "success": False,
+        "msg": "Human not detected",
+        "edges": None,
+        "result": None
+    }
+
+    output, mask = extract_edges(cropped_img, session=session)
     
+    # extract edges from mask
     edges = feature.canny(mask, sigma=5)
-    edges = filters.gaussian(edges, sigma=0.3)
     edges = (edges * 255).astype(np.uint8)
 
     kernel = np.ones((3, 3), np.uint8)
     dilated_edges = cv2.dilate(edges, kernel, iterations=1)
-
     dilated_edges_rgba = cv2.cvtColor(dilated_edges, cv2.COLOR_GRAY2RGBA)
-
-    alpha_factor = 0.7
+    alpha_factor = 0.8
     dilated_edges_rgba[:, :, 3] = (dilated_edges * alpha_factor).astype(np.uint8)
-    #dilated_edges_rgba[:, :, 3] = dilated_edges
 
     output_with_edges = cv2.addWeighted(output, 1.0, dilated_edges_rgba, 1.0, 0)
-    
+
+    result_full_size = paste_on_original_size(output_with_edges, img.shape[:2], bbox)
+    edges_full_size = paste_on_original_size(dilated_edges_rgba, img.shape[:2], bbox, is_edge=True)
+
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as dilated_tempfile:
-        cv2.imwrite(dilated_tempfile.name, dilated_edges_rgba)
+        cv2.imwrite(dilated_tempfile.name, edges_full_size)
         dilated_tempfile_path = dilated_tempfile.name
 
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as output_tempfile:
-        cv2.imwrite(output_tempfile.name, output_with_edges)
+        cv2.imwrite(output_tempfile.name, result_full_size)
         output_tempfile_path = output_tempfile.name
 
     return {
+        "success": True,
+        "msg": None,
         "edges": os.path.basename(dilated_tempfile_path),
         "result": os.path.basename(output_tempfile_path)
     }
